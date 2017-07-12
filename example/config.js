4acaa7f2bb823d7298a1326063d85cc40e695116
ziaochina <ziaochina@gmail.com>
2017-07-12 12:51:48 +0800

    zlj

---
+++ example/config.js
@@ -0,0 +1,14 @@
+var _options
+
+function config(options) {
+	_options = options
+	_options.targetDomId = 'app' //app
+	_options.startAppName = 'xr-template-portal' //xr-template-root
+	//options.apps['xr-template-root'].config({defaultAppName:'app-demo'})
+
+	return _options
+}
+
+config.getCurrent = () => _options
+
+export default config
---
+++ example/index.js
@@ -0,0 +1,27 @@
+import { config, start, componentFactory } from 'xr-meta-engine'
+import myConfig  from './config'
+
+import about from './src/apps/about/index.js'
+import app1 from './src/apps/app1/index.js'
+import app2 from './src/apps/app2/index.js'
+import xr_template_portal from './src/apps/xr-template-portal/index.js'
+
+const apps = {
+	[about.name]:about,	
+	[app1.name]:app1,	
+	[app2.name]:app2,	
+	[xr_template_portal.name]:xr_template_portal,	
+}
+
+
+config(myConfig({apps}))
+
+
+import * as xrComponents from 'xr-component'
+
+Object.keys(xrComponents).forEach(key=>{
+	componentFactory.registerComponent(key, xrComponents[key])
+})
+	
+
+start()
